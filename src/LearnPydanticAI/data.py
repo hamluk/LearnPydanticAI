12e8024f1a2c132dc679111e17f8ca3c66e8bade
Lukas Hamm <lukas@lukashamm.dev>
2025-10-16 18:38:47 +1100

    * adds dictionary to simulate data * adds function tools to fetch and modify data to ai agent

---
+++ src/LearnPydanticAI/data.py
@@ -0,0 +1,4 @@
+PROJECTS = {
+    "LearnPydanticAI": {"group": "dev_team", "open_tasks": 2},
+    "InternalResearch": {"group": "data_team", "open_tasks": 4}
+}
--- src/LearnPydanticAI/model.py
+++ src/LearnPydanticAI/model.py
@@ -7,3 +7,5 @@ class TaskModel(BaseModel):
     priority: int = Field(description="The priority of the task on a scale from 1 (high) to 5 (low).")
     project: str = Field(description="The project the task belongs to.")
     created_by:str = Field(description="The name of the person who created the task.")
+    number_of_open_tasks: int = Field(description="The number of open tasks for the project after creating this task.")
+    success: bool = Field(description="Indicates if the task was created successfully.")
